76d369fb479fd2dfda389b7a5f2b8c76b7a4409a
admin <tmdco12@naver.com>
2024-03-24 13:33:16 +0900

    거듭제곱(power) 계산

--- divmod.py
+++ divmod.py
@@ -1,6 +1,12 @@
+# 연산자가 아닌 함수로 계산
 x = 10
 y = 3
 quotient, remainder = divmod(x, y)  # divmod 함수를 사용하여 x를 y로 나눈 몫과 나머지를 구합니다.
 # divmod 함수는 몫과 나머지를 한 번에 계산하여 튜플 형태로 반환합니다.
 # quotient 변수에는 몫이, remainder 변수에는 나머지가 할당됩니다.
 print(quotient, remainder)  # 몫과 나머지를 출력합니다. 3  1
+
+# 거듭제곱(power) 계산
+# 2의 7승의 계산
+a = 2 ** 7
+print(a) # 128
