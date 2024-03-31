c0c05ac1767d3944404da554df5ec5dec71d4a62
admin <tmdco12@naver.com>
2024-04-01 00:20:40 +0900

    동전 던지기 게임

--- conditional_statement.py
+++ conditional_statement.py
@@ -62,3 +62,14 @@ answer = int(input(f"{x} + {y} = "))
 flag = (answer == (x+y))
 print(flag)
 print("-------------------------------------")
+
+# 동전 던지기 게임
+import random
+
+print("동전 덩지기 게임을 시작합니다.")
+coin = random.randrange(2) # 0 아니면 1
+if coin == 0 :
+    print("앞면입니다.")
+else:
+    print("뒷면입니다.")
+print("게임이 종료되었습니다.")
