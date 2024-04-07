656560f0de374785ff23dd57d669fba12aa7d0be
admin <tmdco12@naver.com>
2024-04-07 20:02:58 +0900

    숫자 맞추기 게임

--- while_for.py
+++ while_for.py
@@ -127,3 +127,25 @@ while i <= 10:
     i = i + 1
 print("합계는",sum)
 
+# 숫자 맞추기 게임
+# 시도 횟수
+# 사용자의 추측값
+# 1 과 100 사이의 난수
+import random
+tries = 0
+guess = 0;
+answer = random.randint(1,100)
+print("1부터 100 사이의 숫자를 맞추시오")
+while guess != answer:
+    guess=int(input("숫자를 입력하시오:"))
+    tries = tries + 1
+    if guess < answer:
+        print("너무 낮음!")
+    elif guess > answer:
+        print("너무 높음!")
+if guess == answer:
+    print("축하합니다. 시도 횟수=",tries)
+else:
+    print("정답은",answer)
+
+
