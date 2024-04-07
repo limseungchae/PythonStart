5369168f5553d706a76e8ce785a40ab5060d155f
admin <tmdco12@naver.com>
2024-04-07 19:21:01 +0900

    팩토리얼 (반대)계산 1 * 2 * 3 * 4 * 5

--- while_for.py
+++ while_for.py
@@ -89,6 +89,8 @@ for i in range(1,10) :
 # 5! = 5 × 4 × 3 × 2 × 1 = 120
 n = int(input("정수를 입력하시오:"))
 fact = 1
-for i in range(1, n+1):
-    fact = fact * i
+# for i in range(1, n+1):
+for i in range(n,0,-1):
+    # fact = fact * i
+    fact *= i
 print(n,"!은",fact,"이다.")
