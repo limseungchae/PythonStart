c7fb13aeebdaa19a2aa4d30c7a878bfe8e907a74
admin <tmdco12@naver.com>
2024-04-07 19:32:41 +0900

    turtle 원하는 수 각형 그리기

--- while_for.py
+++ while_for.py
@@ -95,3 +95,18 @@ for i in range(n,0,-1):
     fact *= i
     print(f"{i} {fact}")
 print(n,"!은",fact,"이다.")
+
+
+# turtle 그리기 - 원하는 수 각형 그리기
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+s = turtle.textinput("","몇각형을 원하시나요?:")
+n = int(s)
+
+for i in range(n):
+    t.forward(100)
+    t.left(360/n)
+
+turtle.done()
