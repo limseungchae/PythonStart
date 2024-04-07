61fb6ad8790b88da59ed5325a2dfdcbe57781453
admin <tmdco12@naver.com>
2024-04-07 20:48:18 +0900

    짝수 출력 위해 2

--- while_for.py
+++ while_for.py
@@ -195,6 +195,7 @@ for co in coffee:
 for i in range(1,11):
     # if i%3 == 0:
     # if i%2 == 0:
-    if i%2 != 0:
+    # if i%2 != 0:
+    if i%2 == 1:
         continue
         print(i,end="")
