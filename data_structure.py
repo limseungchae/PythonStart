7f5797dacce8e3dae1f0874f425820956589a004
admin <tmdco12@naver.com>
2024-05-11 16:20:14 +0900

    교집합

--- data_structure.py
+++ data_structure.py
@@ -143,3 +143,9 @@ set1 = {1, 2, 3}
 set2 = {3, 4, 5}
 union_set = set1 | set2  # 또는 union_set = set1.union(set2)
 print(union_set)  # 출력: {1, 2, 3, 4, 5}
+
+# 교집합
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+intersection_set = set1 & set2  # 또는 intersection_set = set1.intersection(set2)
+print(intersection_set)  # 출력: {3}
